05b9a865aa259982e71379625ca327d0dd4bd6a1
yamamoto345 <mtbook345@gmail.com>
2019-09-17 02:47:36 +0900

    表組みまで

--- js/main.js
+++ js/main.js
@@ -1,23 +1,58 @@
-$(function(){
+// 楽天APIを使って自分で決めた商品ジャンル3つの
+// ランキングデータ10位までを
+// json形式で取得しテーブルで表示する
+
+// 下記4つの方式で作成すること
+// 1.XMLHttpRequest();
+// 2.jQueryによるAjax通信
+// 3.fetchを用いたAjax通信
+// 4.Promiseを用いた非同期通信
+
+// HTML部分
+// 1.○○○○ //商品ジャンル1　←クリック
+// 2.○○○○ //商品ジャンル2　←クリック
+// 3.○○○○ //商品ジャンル3　←クリック
+// //上記のどれかをクリックしたら下記の表示がでる
+
+// 順位	画像	商品名	値段	内容
+// 1				
+// 2				
+// 3				
+// 4				
+// 5				
+// 6				
+// 7				
+// 8				
+// 9				
+// 10			
+
+
+$(function () {
   // buttonがclickされたとき、変数に検索する値を代入
-  $('#search_button1, #search_button2, #search_button3').on('click', function(){
+  $('#search_button1, #search_button2, #search_button3').on('click', function () {
     var keyword = $(this).val();
 
     // リクエストURLを設定する
-    $.get('https://app.rakuten.co.jp/services/api/IchibaItem/Search/20140222?', {
-      applicationId: "xxxx",
-      keyword: keyword,
-      hits: 10
+    $.get('https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?', {
+      applicationId: "xxxxx"
 
-    // 結果が帰ってきたらここでそれを受け取り、空でなければ順番に出力していく
-    }, function(data){
-      if (data.count > 0){
-        console.log(data);
-        $.each(data.Items, function(i, item){
-          var temp = $(`<li><a href="${item.Item.itemUrl}"><img src="${item.Item.mediumImageUrls[0].imageUrl}"></a></li>`);
-          $('ul').append(temp);
-        }) // each
-      } // if
+      // 結果が帰ってきたらここでそれを受け取り、空でなければ順番に出力していく
+    }, function (data) {
+
+      // 配列に格納
+      let rrArry = [];
+
+      console.log(data);
+      for (let i = 0; i < 10; i++) {
+        rrArry.push('<tr><td>' + (i + 1) + '</td>'
+        + '<td>' + data.Items[i].Item.itemName + '</td>'
+        + '<td><img src="' + data.Items[i].Item.mediumImageUrls[0].imageUrl + '"></td>'
+        + '<td>' + data.Items[i].Item.itemPrice + '円</td>'
+        + '<td>' + (data.Items[i].Item.itemCaption).substr(0,100) + '</td></tr>' );
+      }
+      $('tr').after(rrArry);
+
+      // }) // each
     }); // function(data)
   }); // clickイベント
 }); // function
