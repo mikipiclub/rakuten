46188b4ab8cd03426ee51f4e81d11aea42d5f443
yamamoto345 <mtbook345@gmail.com>
2019-09-29 00:14:57 +0900

    jQuery 追加

--- js/jquery.js
+++ js/jquery.js
@@ -31,10 +31,14 @@ $(function () {
   // buttonがclickされたとき、変数に検索する値を代入
   $('#search_button1, #search_button2, #search_button3').on('click', function () {
     var keyword = $(this).val();
+    console.log(keyword)
 
     // リクエストURLを設定する
     $.get('https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?', {
-      applicationId: "xxxxx"
+      applicationId: "xxxxxxxxxx",
+      format: 'json',
+      genreId: keyword
+
 
       // 結果が帰ってきたらここでそれを受け取り、空でなければ順番に出力していく
     }, function (data) {
@@ -43,14 +47,22 @@ $(function () {
       let rrArry = [];
 
       console.log(data);
+      rrArry.push(`<tr>
+                    <th>順位</th>
+                    <th>画像</th>
+                    <th>商品名</th>
+                    <th>値段</th>
+                    <th>内容</th>
+                  </tr>`);
+
       for (let i = 0; i < 10; i++) {
-        rrArry.push('<tr><td>' + (i + 1) + '</td>'
-        + '<td>' + data.Items[i].Item.itemName + '</td>'
-        + '<td><img src="' + data.Items[i].Item.mediumImageUrls[0].imageUrl + '"></td>'
-        + '<td>' + data.Items[i].Item.itemPrice + '円</td>'
-        + '<td>' + (data.Items[i].Item.itemCaption).substr(0,100) + '</td></tr>' );
-      }
-      $('tr').after(rrArry);
+        rrArry.push(`<tr><td>${(i + 1)}</td>
+          <td>${data.Items[i].Item.itemName}</td>
+          <td><img src="${data.Items[i].Item.mediumImageUrls[0].imageUrl}"></td>
+          <td>${data.Items[i].Item.itemPrice}円</td>
+          <td>${(data.Items[i].Item.itemCaption).substr(0,100)}</td></tr>;
+      `)}
+      $('tbody').html(rrArry);
 
       // }) // each
     }); // function(data)
